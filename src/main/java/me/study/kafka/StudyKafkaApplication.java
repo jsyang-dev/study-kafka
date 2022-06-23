8ac796f22fd9523accf112f4a57cd6b3daf5b9a0
Jeongsu Yang <mycat83@gmail.com>
2022-06-23 15:43:36 +0900

    Initial commit

---
+++ src/main/java/me/study/kafka/StudyKafkaApplication.java
@@ -0,0 +1,13 @@
+package me.study.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyKafkaApplication.class, args);
+    }
+
+}

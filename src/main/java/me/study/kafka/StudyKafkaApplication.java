33e69c0d044804a0506329bde78f6b7942ea9d86
Jeongsu Yang <mycat83@gmail.com>
2022-06-24 15:36:35 +0900

    feat: Kafka Topic

--- src/main/java/me/study/kafka/StudyKafkaApplication.java
+++ src/main/java/me/study/kafka/StudyKafkaApplication.java
@@ -1,7 +1,15 @@
 package me.study.kafka;
 
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.TopicDescription;
+import org.apache.kafka.clients.admin.TopicListing;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Collections;
+import java.util.Map;
 
 @SpringBootApplication
 public class StudyKafkaApplication {
@@ -10,4 +18,21 @@ public class StudyKafkaApplication {
         SpringApplication.run(StudyKafkaApplication.class, args);
     }
 
+    @Bean
+    public ApplicationRunner runner(AdminClient adminClient) {
+        return args -> {
+            Map<String, TopicListing> topics = adminClient.listTopics().namesToListings().get();
+            for (String topicName : topics.keySet()) {
+                TopicListing topicListing = topics.get(topicName);
+                System.out.println(topicListing);
+
+                Map<String, TopicDescription> description = adminClient.describeTopics(Collections.singleton(topicName)).all().get();
+                System.out.println(description);
+
+                if (!topicListing.isInternal()) {
+                    adminClient.deleteTopics(Collections.singleton(topicName));
+                }
+            }
+        };
+    }
 }
